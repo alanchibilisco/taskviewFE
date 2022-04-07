a10e4495b2740cd66b6f2d976c172e4160e44a60
alanChibilisco <alan.chibilisco@gmail.com>
2022-04-07 20:42:00 -0300

    se agrego funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Container, Row } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
-import Task from './Task';
+import { Col, Container, Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import Task from "./Task";
+import ViewTask from "./ViewTask";
+//import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
 
 function App() {
-  const urlTask=process.env.REACT_APP_API_TASK;
-  const urlPutTask=process.env.REACT_APP_API_PUTTASK;
-  const[listTask, setListTask]=useState([]);
-  const getListTask=async()=>{
+  const urlTask = process.env.REACT_APP_API_TASK;
+  const urlPutTask = process.env.REACT_APP_API_PUTTASK;
+  const [listTask, setListTask] = useState([]);
+  const [show, setShow] = useState(false);
+  const [task, setTask]=useState({});
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const getListTask = async () => {
     try {
-      const res=await fetch(urlTask);
-      const resJson=await res.json();
+      const res = await fetch(urlTask);
+      const resJson = await res.json();
       console.log(resJson);
       setListTask(resJson);
     } catch (error) {
       console.log(error);
     }
   };
-  useEffect(()=>{
+  useEffect(() => {
     getListTask();
-  },[])
-  console.log(urlTask);
-  console.log(urlPutTask);
-  console.log(listTask);
+  }, []);
+  // console.log(urlTask);
+  // console.log(urlPutTask);
+  // console.log(listTask);
+  console.log(task);
   return (
     <div className="container my-5">
-      <h1 className='fw-bold'>List view</h1>
-      <Container className='my-5'>
+      <h1 className="fw-bold">List view</h1>
+      <Container className="my-5">
         <Row>
-          {listTask.map((task)=>(
-            <Col xs={12} md={4} key={task.id}><Task task={task}></Task></Col>
-          ))}       
+          {listTask.map((task) => (
+            <Col xs={12} md={4} key={task._id}>
+              <Task task={task} handleShow={handleShow} setTask={setTask}></Task>
+            </Col>
+          ))}
         </Row>
       </Container>
+      <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>TASK: {task.UUID}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Title: {task.title}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </>
     </div>
   );
 }
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
 
-const Task = ({task}) => {
-    return (
-        <div className='border rounded bg-primary container py-2 mx-2'>
-            <h4 className='my-2'>Task UUID:{task.UUID}</h4>
-            <p className='text-break'>Title:{task.title}</p>
-        </div>
-    );
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Task = ({ task, handleShow, setTask }) => {  
+ const newTask=()=>{
+     setTask({
+         UUID: task.UUID,
+         title: task.title
+     })
+ };
+  return ( 
+    <div className="border rounded bg-primary container py-2">
+      <Button onClick={()=>{
+          handleShow();
+          newTask();
+      }}><h4 className="my-2 text-dark">Task UUID:{task.UUID}</h4></Button>      
+      <p className="text-break">Title:{task.title}</p>        
+    </div>    
+  );
 };
 
 export default Task;
